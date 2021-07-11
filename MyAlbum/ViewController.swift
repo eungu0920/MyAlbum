64142fe51f2f4472b22625fff56ca12c4cada729
eungu <eungu0920@naver.com>
2021-07-11 15:52:21 +0900

    Initial Commit

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 은구 on 2021/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
